fbb35638af28bab51d4ba8f1d9183c49f633e3c8
YasserYka <yasseryka@gmail.com>
2020-02-24 01:31:10 +0300

    cached pool and cleaned unused impoerts

--- src/side/server/Server.java
+++ src/side/server/Server.java
@@ -1,15 +1,13 @@
 package side.server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Optional;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
-import request.RequestHandler;
 import side.client.HandleClient;
 
 public class Server {
@@ -21,6 +19,7 @@ public class Server {
 
 	private static ServerSocket socket = null;
 	private final static int PORT = 5555;
+	private static ThreadPoolExecutor POOL = (ThreadPoolExecutor) Executors.newCachedThreadPool();
 	
 	private Server() {}
 	
@@ -37,9 +36,8 @@ public class Server {
 	
 	private static void accept() {
 		try {	
-			Thread thread = new Thread(new HandleClient(socket.accept()));
+			POOL.execute(new HandleClient(socket.accept()));
 			System.out.println("R: CLIENT CONNECTED AND THREAD CREATED FOR IT");
-			thread.start();
 		}catch (IOException e){/*LOG IT*/}
 		
 	}
